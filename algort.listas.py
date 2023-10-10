48a8dc515b415255bf665573284c15f177343b89
Juan Diego <juandiego8520@gmail.com>
2023-10-09 19:46:01 -0500

    le cambie a diego

---
+++ algort.listas.py
@@ -0,0 +1,22 @@
+def diegoElemenListPos(lst, elem):
+
+    #FUNCION QUE BUSCA UN ELEMENTO EN LA LISTA
+    #SI LO ENCUENTRA DEVUELVE LA POSICION
+    #SI NO LO ENCUENTRA DEVUELVE -1
+
+    for p in range(len(lst)):
+        if elem == lst[p]:   #si element es lista en la posicion p
+            return p
+        
+    return -1
+
+def existElemList(lst,elem):
+    #FUNCION QUE BUSCA UN ELEMENTO EN LA LISTA
+    #SI LO ENCUENTRA DEVUELVE TRUE
+    #SI NO LO ENCUENTRA DEVUELVE FALSE
+
+    for e in lst:
+        if e ==elem:
+            return True
+    return False
+
